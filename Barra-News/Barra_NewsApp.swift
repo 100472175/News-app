4391e054718bebcb8a8e8b3bccf8de21402b5faf
100472175 <100472175@alumnos.uc3m.es>
2023-03-07 14:05:08 +0100

    Initial Commit

---
+++ Barra-News/Barra_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barra_NewsApp.swift
+//  Barra-News
+//
+//  Created by Eduardo Alarcon on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Barra_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
